c08958599ca7ce023dbfe8ecece066cfe72a248a
LuTri <tristan.lucas.de@gmail.com>
2015-06-10 08:20:40 +0200

    SCHORN: ladderboard

---
+++ ladderboard.h
@@ -0,0 +1,26 @@
+/*
+========================================================================
+Autor       : GruppeB (Kevin Schorn)
+Firma       : HHBK
+Dateiname   : ladderboard.c
+Datum       : 08.06.15	
+Beschreibung: Darstellung der Bestenliste.
+Version     : 1.0
+Programmschnittstelle: 
+========================================================================
+*/
+
+/*
+========================================================================
+Include Dateien
+========================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "db.h"
+void Bestenliste();
+void PersListe();
+
+
