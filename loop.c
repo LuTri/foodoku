fa1c072a1548b71b0d18c7e2d5d689713464132e
LuTri <tristan.lucas.de@gmail.com>
2015-06-11 21:15:49 +0200

    LUCAS: mod loop.c merging

--- loop.c
+++ loop.c
@@ -13,183 +13,181 @@ Beschreibung: Aufrufen und Benutzung des UI
 
 
 //INCLUDE
+#include <time.h>
+#include "db.h"
 #include "tableui.h"
 #include "menue.h"
 
 //Präprozessorkonstanten
 
-#define KEY_K 107
-#define KEY_Q 11
-#define KEY_F 102
-#define KEY_R 114
+#define KEY_K 'r'
+#define KEY_Q 'q'
+#define KEY_F 'f'
+#define KEY_R 'r'
 #define KEY_ENTER 13
-#define NUM_1 49
-#define NUM_2 50
-#define NUM_3 51
-#define NUM_4 52
-#define NUM_5 53
-#define NUM_6 54
-#define NUM_7 55
-#define NUM_8 56
-#define NUM_9 57
+#define NUM_1 '0'+1
+#define NUM_2 '0'+2
+#define NUM_3 '0'+3
+#define NUM_4 '0'+4
+#define NUM_5 '0'+5
+#define NUM_6 '0'+6
+#define NUM_7 '0'+7
+#define NUM_8 '0'+8
+#define NUM_9 '0'+9
 
 
 
 void game_loop(int iSchwierigkeit, int iSpielart, int iUserId)
-/*
-	Erstellen des Sudoku-Feldes
-	1. Parameter:	iSchwierigkeitsgrad
-	2. Parameter:	Spielart (Ranglisten oder freies Spiel)
-	3. Parameter:	User ID (für die Bestenliste)
-*/
+   /*
+   Erstellen des Sudoku-Feldes
+   1. Parameter:	iSchwierigkeitsgrad
+   2. Parameter:	Spielart (Ranglisten oder freies Spiel)
+   3. Parameter:	User ID (für die Bestenliste)
+   */
 {
 
-// Variablendeklaration
+   // Variablendeklaration
 
-	/*Aktuelle Position, aktueller Spielstatus(0 oder 1), Hilfeanzeige (0 oder 1),
-	  Ranglisten-Spiel (0 oder 1), aktueller Tastendruck, Zeitvariablen
-	*/
+   /*Aktuelle Position, aktueller Spielstatus(0 oder 1), Hilfeanzeige (0 oder 1),
+   Ranglisten-Spiel (0 oder 1), aktueller Tastendruck, Zeitvariablen
+   */
 
-	int		iPlaying,
-			iHelp = 0,
-			iX,
-			iY;
+   int		iPlaying,
+      iHelp = 0,
+      iX,
+      iY;
 
-	char	cRanked,
-			cTaste,
-			cTempTaste;
+   char	cRanked,
+      cTaste = 0,
+      cTempTaste = 0;
 
-	time_t	tBeg,
-			tEnd;
+   time_t	tBeg,
+      tEnd;
 
-	// Gamedata-Struktur
+   // Gamedata-Struktur
 
-	typedef struct {
-	   int iUserId;
-	   int iMode;
-	   int iSeconds;
-	   int iHelps;
-	   int iFilled;
-	   char cFinished;
-	   char cRanked;
-	   char* sDate;
-	} GAME;
+   typedef struct {
+      int iUserId;
+      int iMode;
+      int iSeconds;
+      int iHelps;
+      int iFilled;
+      char cFinished;
+      char cRanked;
+      char* sDate;
+   } GAME;
 
-	GAME this_game;
-
-
-
-	
-	//Zuweisung von User ID und Schwierigkeitsgrad in die Gamedata-Struktur
-
-	this_game.iUserId	= iUserId;
-	this_game.iMode		= iSchwierigkeit;
+   GAME this_game;
+   this_game.iUserId = iUserId;
 
 
-//Spielaufbau
 
-		startup_sudoku();
+   //Zuweisung von User ID und Schwierigkeitsgrad in die Gamedata-Struktur
 
-		tBeg = time(&tBeg);
-
-//Rendering-Loop
-		while(iPlaying){
-
-			//Prüfung ob Ranglisten-Spiel oder nicht
-			if(iSpielart == 1 && iUserId != 0){
-				this_game.cRanked = 1;
-			}else{
-				this_game.cRanked = 0;
-			}
-
-//Spielfeld anzeigen
-		show_ui(iHelp);
-			
-	
-//Abfangen der Tastendrücke
-		cTaste = get_input();
-
-				if(cTaste == KEY_K)
-				//Vorschläge für Feld (K)
-				{
-					get_cursor_pos(&iX, &iY);
-					showValuesForHelp(cSudoku, iX, iY);
-					this_game.iHelps++;
-				}
+   this_game.iUserId	= iUserId;
+   this_game.iMode		= iSchwierigkeit;
 
 
-				if(cTaste == KEY_Q)
-				//Quit (Q)
-				{
-					iPlaying = 0;
-					break;
-				}
+   //Spielaufbau
 
-				if(cTaste == KEY_F)
-				//Einzelnes Feld füllen (F)
-				{
-					this_game.iFilled++;
-				}
+   startup_sudoku();
 
+   tBeg = time(&tBeg);
 
-				if(cTaste == KEY_ENTER)
-				//Eingabe (ENTER)
-				{
-					if(cTempTaste != 0){
-						get_cursor_pos(&iX, &iY);
-						cSudoku[iY][iX] = cTempTaste;
-						cTempTaste = 0;
-					}
-				}
+      //Prüfung ob Ranglisten-Spiel oder nicht
+      if(iSpielart == 1 && iUserId != 0){
+         this_game.cRanked = 1;
+      }else{
+         this_game.cRanked = 0;
+      }
 
+   //Rendering-Loop
+   while(1){
+      //Spielfeld anzeigen
+      show_ui(iHelp);
 
-				if(cTaste == KEY_R)
-				//Regeln (R)
-				{
-					if(iHelp == 0){
-						iHelp = 1;
-					}else{
-						iHelp = 0;
-					}
-				}
+      //Abfangen der Tastendrücke
+      cTaste = get_input();
 
-				if(		cTaste == NUM_1
-					||	cTaste == NUM_2
-					||	cTaste == NUM_3
-					||	cTaste == NUM_4
-					||	cTaste == NUM_5
-					||	cTaste == NUM_6
-					||	cTaste == NUM_7
-					||	cTaste == NUM_8
-					||	cTaste == NUM_9
-				)
-				//ZAHLEN(1-9)
-				{
-					cTempTaste = cTaste;
-				}
+      if(cTaste == KEY_K)
+         //Vorschläge für Feld (K)
+      {
+         get_cursor_pos(&iX, &iY);
+         //	showValuesForHelp(cSudoku, iX, iY);
+         this_game.iHelps++;
+      }
 
 
-				if(		cTaste == CURS_UP 
-					||	cTaste == CURS_DOWN
-					||	cTaste == CURS_LEFT
-					||	cTaste == CURS_RIGHT
-					||	cTaste == CURS_RIGHT
-				)
-				//PFEILTASTEN
-				{
-					move_cursor(cTaste);
-				}
-		}
+      if(cTaste == KEY_Q)
+         //Quit (Q)
+      {
+         iPlaying = 0;
+         break;
+      }
 
-// Berechnung der Spieldauer
-		tEnd = time(&tEnd);
-		this_game.iSeconds = (int) difftime(tBeg, tEnd);
-
-// Insert in Bestenliste
-		if(this_game.iUserId != 0){
-			insert_game_data(&this_game);
-		}
+      if(cTaste == KEY_F)
+         //Einzelnes Feld füllen (F)
+      {
+         this_game.iFilled++;
+      }
 
 
+      if(cTaste == KEY_ENTER)
+         //Eingabe (ENTER)
+      {
+         if(cTempTaste != 0){
+            get_cursor_pos(&iX, &iY);
+            cSudoku[iY][iX] = cTempTaste - '0';
+            cTempTaste = 0;
+         }
+      }
+
+
+      if(cTaste == KEY_R)
+         //Regeln (R)
+      {
+         if(iHelp == 0){
+            iHelp = 1;
+         }else{
+            iHelp = 0;
+         }
+      }
+
+      if(		cTaste == NUM_1
+         ||	cTaste == NUM_2
+         ||	cTaste == NUM_3
+         ||	cTaste == NUM_4
+         ||	cTaste == NUM_5
+         ||	cTaste == NUM_6
+         ||	cTaste == NUM_7
+         ||	cTaste == NUM_8
+         ||	cTaste == NUM_9
+         )
+         //ZAHLEN(1-9)
+      {
+         cTempTaste = cTaste;
+      }
+
+
+      if(		cTaste == CURS_UP 
+         ||	cTaste == CURS_DOWN
+         ||	cTaste == CURS_LEFT
+         ||	cTaste == CURS_RIGHT
+         ||	cTaste == CURS_RIGHT
+         )
+         //PFEILTASTEN
+      {
+         move_cursor(cTaste);
+      }
+   }
+
+   // Berechnung der Spieldauer
+   tEnd = time(&tEnd);
+   this_game.iSeconds = (int) difftime(tBeg, tEnd);
+   shutdown_sudoku();
+   // Insert in Bestenliste
+   if(this_game.iUserId != 0){
+      insert_game_data(&this_game);
+   }
 }
 
