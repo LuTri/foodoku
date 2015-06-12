58089127ae6d962c3b5292e37bc01bf2c7d107ae
LuTri <tristan.lucas.de@gmail.com>
2015-06-12 23:31:31 +0200

    LUCAS: format and bracket bug in loop.c

--- loop.c
+++ loop.c
@@ -51,11 +51,11 @@ void game_loop(int iSchwierigkeit, int iSpielart, int iUserId)
    */
 {
 
-// Variablendeklaration
+   // Variablendeklaration
 
-/*Aktuelle Position, aktueller Spielstatus(0 oder 1), Hilfeanzeige (0 oder 1),
-Ranglisten-Spiel (0 oder 1), aktueller Tastendruck, Zeitvariablen
-*/
+   /*Aktuelle Position, aktueller Spielstatus(0 oder 1), Hilfeanzeige (0 oder 1),
+   Ranglisten-Spiel (0 oder 1), aktueller Tastendruck, Zeitvariablen
+   */
 
    int iPlaying = 1, iHelp = 0, iX, iY;
 
@@ -63,14 +63,14 @@ Ranglisten-Spiel (0 oder 1), aktueller Tastendruck, Zeitvariablen
    int y;
 
    char	cRanked,
-         cTaste = 0,
-         cTempTaste = 0;
+      cTaste = 0,
+      cTempTaste = 0;
 
    time_t tBeg,
-          tEnd;
+      tEnd;
 
 
-// Gamedata-Struktur
+   // Gamedata-Struktur
 
    typedef struct {
       int iUserId;
@@ -88,11 +88,11 @@ Ranglisten-Spiel (0 oder 1), aktueller Tastendruck, Zeitvariablen
 
 
    for(y = 0; y < 9; y++){
-	   for(x = 0; x < 9; x++){
-		   cShownSudoku[y][x] = 0;
-	   }
+      for(x = 0; x < 9; x++){
+         cShownSudoku[y][x] = 0;
+      }
    }
-   
+
    cShownSudoku[4][5] = 4;
    cShownSudoku[1][2] = 6;
    cShownSudoku[3][7] = 2;
@@ -100,7 +100,7 @@ Ranglisten-Spiel (0 oder 1), aktueller Tastendruck, Zeitvariablen
 
    prepare_start_sudoku();
 
-//Zuweisung von User ID und Schwierigkeitsgrad in die Gamedata-Struktur
+   //Zuweisung von User ID und Schwierigkeitsgrad in die Gamedata-Struktur
 
    this_game.iHelps = 0;
    this_game.iFilled	= 0;
@@ -108,99 +108,97 @@ Ranglisten-Spiel (0 oder 1), aktueller Tastendruck, Zeitvariablen
    this_game.iMode = iSchwierigkeit;
 
 
-//Spielaufbau
+   //Spielaufbau
 
    startup_sudoku();
    create_sudoku();
 
    tBeg = time(&tBeg);
 
-//Prüfung ob Ranglisten-Spiel oder nicht
-      if(iSpielart == 1 && iUserId != 0){
-         this_game.cRanked = 1;
-      }else{
-         this_game.cRanked = 0;
-      }
+   //Prüfung ob Ranglisten-Spiel oder nicht
+   if(iSpielart == 1 && iUserId != 0){
+      this_game.cRanked = 1;
+   }else{
+      this_game.cRanked = 0;
+   }
 
-//Rendering-Loop
+   //Rendering-Loop
    while(iPlaying){
-//Spielfeld anzeigen
+      //Spielfeld anzeigen
       show_ui(iHelp, 0);
 
-//Abfangen der Tastendrücke
+      //Abfangen der Tastendrücke
       cTaste = get_input();
 
-//Hier werden den einzelnen Tastendrücken Anweisungen zugewiesen
-	  switch(cTaste)
-	  {
-//Tasten K für Kandidaten, Q zum Beenden, F um das aktuelle Feld zu lösen, R für die Regeln, Enter für die Eingabe
-		case KEY_K:
-			get_cursor_pos(&iY, &iX);
-			this_game.iHelps++;
-		break;
+      //Hier werden den einzelnen Tastendrücken Anweisungen zugewiesen
+      switch(cTaste)
+      {
+         //Tasten K für Kandidaten, Q zum Beenden, F um das aktuelle Feld zu lösen, R für die Regeln, Enter für die Eingabe
+      case KEY_K:
+         get_cursor_pos(&iY, &iX);
+         this_game.iHelps++;
+         break;
 
-		case KEY_Q:
-			iPlaying = 0;
-		break;
+      case KEY_Q:
+         iPlaying = 0;
+         break;
 
-		case KEY_F:
-			this_game.iFilled++;
-		break;
+      case KEY_F:
+         this_game.iFilled++;
+         break;
 
-		case KEY_ENTER:
+      case KEY_ENTER:
 
-			get_cursor_pos(&iX, &iY);
+         get_cursor_pos(&iX, &iY);
 
-			if(cTempTaste != 0 && check_input(iX, iY)){
-				
-				cShownSudoku[iY][iX] = cTempTaste - '0';
-				cTempTaste = 0;
-			}
-		break;
+         if(cTempTaste != 0 && check_input(iX, iY)){
 
-		case KEY_R:
-			if(iHelp == 0){
-				iHelp = 1;
-			 }else{
-				iHelp = 0;
-			 }
-		break;
+            cShownSudoku[iY][iX] = cTempTaste - '0';
+            cTempTaste = 0;
+         }
+         break;
 
-//Eingabe der Zahlen 1-9
-		case NUM_1:
-		case NUM_2:
-		case NUM_3:
-		case NUM_4:
-		case NUM_5:
-		case NUM_6:
-		case NUM_7:
-		case NUM_8:
-		case NUM_9:
-			cTempTaste = cTaste;
-		break;
+      case KEY_R:
+         if(iHelp == 0){
+            iHelp = 1;
+         }else{
+            iHelp = 0;
+         }
+         break;
 
-//Navigation durch Pfeiltasten
-		case CURS_UP:
-		case CURS_DOWN:
-		case CURS_LEFT:
-		case CURS_RIGHT:
-			move_cursor(cTaste);
-			cTempTaste = 0;
-		break;
+         //Eingabe der Zahlen 1-9
+      case NUM_1:
+      case NUM_2:
+      case NUM_3:
+      case NUM_4:
+      case NUM_5:
+      case NUM_6:
+      case NUM_7:
+      case NUM_8:
+      case NUM_9:
+         cTempTaste = cTaste;
+         break;
 
-	  }
+         //Navigation durch Pfeiltasten
+      case CURS_UP:
+      case CURS_DOWN:
+      case CURS_LEFT:
+      case CURS_RIGHT:
+         move_cursor(cTaste);
+         cTempTaste = 0;
+         break;
 
-// Berechnung der Spieldauer
+      }
+
+   }
+   // Berechnung der Spieldauer
    tEnd = time(&tEnd);
    this_game.iSeconds = (int) difftime(tEnd, tBeg);
 
    show_result(this_game.iHelps, this_game.iFilled, this_game.iSeconds);
    shutdown_sudoku();
-// Insert in Bestenliste
-  /* if(this_game.iUserId != 0){
+   // Insert in Bestenliste
+   if(this_game.iUserId != 0){
       insert_game_data(&this_game);
-   }*/
-
+   }
 }
-}
-
