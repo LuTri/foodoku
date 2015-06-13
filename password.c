58920719a75c73f5e3c1e729ee9cbafe23ff46ab
LuTri <tristan.lucas.de@gmail.com>
2015-06-13 16:37:03 +0200

    LUCAS: commenting on password.*

--- password.c
+++ password.c
@@ -1,17 +1,47 @@
+/* 
+   ============================================================================
+   Programmname: password.c
+   Autor       : Tristan Lucas
+                 Heinrich-Hertz-Berufskolleg
+   Datum       : 12.06.2015
+   Thema       : Sudoku
+   Version     : 1.0
+   Beschreibung: Stellt eine Eingabe-verschleiernde Funktion zur Passwort-
+                 Eingabe bereit
+
+   Programmschnittstelle: Siehe zugehörige Header-Datei  
+   ============================================================================ 
+*/
 #include "password.h"
 #include <conio.h>
 
 void passwd_input(char cBuff[200])
+/* 
+   ============================================================================
+   Liest ein maximal 200 Zeichen langes Passwort ein und verschleiert dabei die
+   Ausgabe mit '*'
+      1. Parameter: Das Array, in dem das Passwort gespeichert werden soll
+   ============================================================================ 
+*/
 {
+   /* Variablen-Definition und Initialisierung */
    char cCharacter;
    int i=0;
+
+   /* Erste Abfang einer Tastatureingabe */
    cCharacter = _getch();
+   /* Für jede gedrückte Taste wirde ein '*' auf dem Bildschirm ausgegeben */
    _putch('*');
    while (cCharacter != '\n' && cCharacter != '\r' && i < 200)
    {
+      /*
+         Solange der Nutzer nicht die Enter-Taste drückt oder das Passwort zu
+         lang wird, wird die Eingabe fortgeführt
+      */
       cBuff[i++] = cCharacter;
       cCharacter = _getch();
       _putch('*');
    }
+   /* Der String wird terminiert */
    cBuff[i] = '\0';
 }
--- password.h
+++ password.h
@@ -1,6 +1,23 @@
+/* 
+   ============================================================================
+   Programmname: password.h
+   Autor       : Tristan Lucas
+                 Heinrich-Hertz-Berufskolleg
+   Datum       : 12.06.2015
+   Thema       : Sudoku
+   Version     : 1.0
+   Beschreibung: Stellt eine Eingabe-verschleiernde Funktion zur Passwort-
+                 Eingabe bereit
+   ============================================================================ 
+*/
 #ifndef _PASSWORD_H
 #define _PASSWORD_H
 
+/* 
+   ============================================================================
+   Funktionsprototyp
+   ============================================================================ 
+*/
 void passwd_input(char cBuff[200]);
 
 #endif
